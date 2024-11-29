3d60c700ca9c2493b37f8a893d48e5102f44db09
Heberton Geovane <heberton.geovane@gmail.com>
2024-11-29 10:40:04 -0300

    Dasafio-02

---
+++ aula-03/Desafio-02/index.js
@@ -0,0 +1,9 @@
+
+let productValue = Number(prompt('Enter the Product value:  '))
+
+if (productValue >= 20){
+    document.getElementById("result").innerHTML = 'Approved' 
+}
+else{
+    document.getElementById("result").innerHTML = 'Denied' 
+}
